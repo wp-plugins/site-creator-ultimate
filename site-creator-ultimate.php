55916690ebd3504d6a1b973f99dfac0a0a4c8518
wpcolor <wpcolor@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-09-19 21:10:45 +0000

    git-svn-id: https://plugins.svn.wordpress.org/site-creator-ultimate/tags/2.2@601276 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ site-creator-ultimate.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+Plugin Name: Site Creator Ultimate
+Plugin URI: http://1customize.com
+Description: Advanced Page & Post Layout Editor For Wordpress
+Version: 2.1.6
+Author: 1customize
+*/
+
+
+
+/**************************************************************************
+
+Copyright (C) 2011-2012 1customize
+
+email : info@1customize.com
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License
+		
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  gnu.org/licenses
+
+**************************************************************************/
+
+
+@include_once('all.php');
+@include_once('lib.php');
+//print_r(error_get_last());
+?>
